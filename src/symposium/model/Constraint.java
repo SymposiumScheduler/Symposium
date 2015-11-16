04caa716ddca77fd2e67834cf7839dedb47411f7
shatterspike1 <stupidinternet303@gmail.com>
2015-11-16 14:28:47 -0700

    Made checkTime logic consistent, added functions; need access to ScheduleData to check Panels to check where Panelists are currently assigned

--- src/symposium/model/Constraint.java
+++ src/symposium/model/Constraint.java
@@ -31,25 +31,32 @@ abstract class TimeConstraint extends Constraint {
     public boolean checkViolationCache(){
         
     }
-    
+
     @Override
-    public boolean isConstraintViolated(VenueTime venueTime){
+    public boolean isConstraintViolated(VenueTime venueTime) {
+        if checkTime(venueTime) {
+            return false;
+        }
+        else {
+            return true;
+        }
     }
     
     private boolean checkTime();
 }
 class NewPanelistConstraint extends TimeConstraint { //Ask team members
+    
     @Override
     private boolean checkTime(){
-        RangeLine time = venueTime.getTime(); // Implement in VenueTime later
-        RangeLine monday = new RangeLine(0, 2400); // Assuming Joey's current parser, change if implementation changes
+        Range time = venueTime.getTime(); // Implement in VenueTime later
+        Range monday = new TimeRange(0, 2400); // Assuming Joey's current parser, change if implementation changes
         short int newMoreThanTwo = 0;
         for (for i = 0; i < panel.PANELIST.length(); i++){
             if panel.PANELIST[i].contains('new'){  // built on assumption that the PANELIST array in panel contains the substring
                 newMoreThanTwo++;
             }
         }
-        if (!time.doesIntersect(monday) || newMoreThanTwo < 2){
+        if (!time.Intersect(monday) || newMoreThanTwo < 2){
             return true;
         }
         else {
@@ -57,7 +64,28 @@ class NewPanelistConstraint extends TimeConstraint { //Ask team members
         }
     }
 }
-class DurationConstraint extends TimeConstraint {}
+/**
+ * Constraint class; its presence in a panel means that the slot it takes up should be short.
+ */
+class DurationConstraint extends TimeConstraint {
+    //May change implementation in future if VenueTime implements an isShort bool
+    /**
+     * Checks the time to see if it counts as short or long.
+     * Dependencies: VenueTime, Range
+     * @return bool true for short or false for long.
+     */
+    @Override
+    private boolean checkTime(VenueTime venueTime) {
+        Range time = venueTime.getTime();
+        if (time.END-time.START < 60) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+    
+}
 
 abstract class NoOverlapConstraint extends Constraint {}
 class PanelistConstraint extends NoOverlapConstraint {}

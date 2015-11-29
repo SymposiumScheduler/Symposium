473bab48b0e071f94e62b1e6cf9e051c406bacf1
shatterspike1 <stupidinternet303@gmail.com>
2015-11-29 14:27:06 -0700

    Added mostly empty test folder (Panel class lacks constructor, so can't write tests) Also replaced every occurance of short int with short.

---
+++ src/symposium/test/model/VenueConstraintTest.java
@@ -0,0 +1,18 @@
+package symposium.model;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+import static org.junit.Assert.*;
+
+public class VenueConstraintTest {
+
+    @Test
+    public void testConstructor() throws Exception {
+        short priority = 1;
+        Panel p = new Panel(); //Can't write tests because Panel class doesn't have constructor.
+    }
+}

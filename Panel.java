af7e3d10cd43a7d9be0636208e688739250cbb19
Yousef <yousef.alsabr@colorado.edu>
2015-11-30 04:36:35 -0700

    Starting Joey and Yousef branch

---
+++ Panel.java
@@ -0,0 +1,68 @@
+import java.util.*;
+
+public class Panel implements Comparable<Panel>{
+    String name;
+    boolean Lock = false;
+    Range availability;
+    List<String> panelists = new ArrayList<String>();
+    List<String> constraints = new ArrayList<String>();
+    VenueTime assignedVenueTime;
+    int difficulty;
+    String category;
+
+    public Panel(String name, Map<String, TimeRangeSeries> panelists, String category, List<String> constraints){
+        this.name = name;
+        this.category = category;
+        this.constraints = constraints;
+
+        int new_count = 0;
+        List<TimeRangeSeries> times = new ArrayList<TimeRangeSeries>(){};
+        for (String panelist : panelists.keySet()){
+            if (panelist.contains("n_")){
+                panelist.replace("n_","");
+                new_count += 1;
+            }
+            this.panelists.add(panelist);
+            times.add(panelists.get(panelist));
+        }
+        if (new_count >= 2){
+            constraints.add("new");
+        }
+
+        int size = times.size();
+        Range start = times.get(0);
+        for (int i = 0; i > size; i++){
+            start = start.intersect(times.get(i+1));
+        }
+        this.availability = start;
+
+        //TODO set lock based on ...?
+
+    }
+
+    public void setDifficulty(int difficulty) {
+        this.difficulty = difficulty;
+        //concurrency - ( (end - start) + (end - start) ....)
+    }
+
+    public int getDifficulty(){
+        return difficulty;
+    }
+
+    public void setDifficulty(){
+        //TODO
+    }
+
+    public VenueTime getVenueTime(){
+        return assignedVenueTime;
+    }
+
+    public void setVenueTime(VenueTime venueTime){
+        this.assignedVenueTime = venueTime;
+    }
+
+    @Override
+    public int compareTo(Panel that) {
+        return this.difficulty - that.difficulty;
+    }
+}
---
+++ PanelTest.java
@@ -0,0 +1,45 @@
+import static org.junit.Assert.*;
+
+//package symposium.model;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+/**
+ * Created by yousef-alsber on 11/30/15.
+ */
+public class PanelTest {
+
+    @org.junit.Test
+    public void testSetDifficulty() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void testGetDifficulty() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void testSetDifficulty1() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void testGetVenueTime() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void testSetVenueTime() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void testCompareTo() throws Exception {
+
+    }
+}

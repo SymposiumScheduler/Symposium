5d86b869a229a3315e5174f15644c6b80d90bf6c
Yousef <yousef.alsabr@colorado.edu>
2016-01-14 19:12:30 -0700

    Fixed ConstraintFactory

--- src/symposium/model/ConstraintFactory.java
+++ src/symposium/model/ConstraintFactory.java
@@ -8,27 +8,27 @@ public class ConstraintFactory{
         List<Constraint> constraints = new ArrayList<Constraint>();
         for (String constraint_string : constraint_strings){
             if (constraint_string.contains("New-Panelist")){
-                int priority = Integer.getInteger(constraint_string.split(":")[1]);
+                int priority = Integer.valueOf(constraint_string.split(":")[1]);
                 short ps = (short) priority;
                 Constraint constraint = new NewPanelistConstraint(ps, panel);
                 constraints.add(constraint);
             }
             else if (constraint_string.contains("Paired-Panelist")){
-                int priority = Integer.getInteger(constraint_string.split(":")[1]);
+                int priority = Integer.valueOf(constraint_string.split(":")[1]);
                 short ps = (short) priority;
                 Constraint constraint = new PairedPanelistConstraint(ps, panel);
                 constraints.add(constraint);
             }
             else if (constraint_string.contains("Single-Category")){
-                int priority = Integer.parseInt(constraint_string.split(":")[1]);
+                int priority = Integer.valueOf(constraint_string.split(":")[1]);
                 short ps = (short) priority;
                 Constraint constraint = new CategoryConstraint(ps, panel);
                 constraints.add(constraint);
             }
             else if (constraint_string.contains("Minimum-Capacity")){
-                int priority = Integer.getInteger(constraint_string.split(":")[1]);
+                int priority = Integer.valueOf(constraint_string.split(":")[1]);
                 short ps = (short) priority;
-                int size =  Integer.parseInt(constraint_string.split("\\(")[1].split("\\)")[0]);
+                int size =  Integer.valueOf(constraint_string.split("\\(")[1].split("\\)")[0]);
                 short s = (short) size;
                 Constraint constraint = new SizeConstraint(ps, panel, s);
                 constraints.add(constraint);

fd2f38366d942de76958ff01563d66610a3795b3
Yousef <yousef.alsabr@colorado.edu>
2016-02-21 14:52:44 -0700

    Added Warning Messages to report

--- src/symposium/Report.java
+++ src/symposium/Report.java
@@ -21,16 +21,23 @@ public class Report {
             result.append("\n\n").append(panel.toString());
         }
 
-        result.append("\n\n---------------------------- Free Panelists ---------------------------");
+        result.append("\n\n---------------------------- Underutilized Panelists ---------------------------");
         Map<String, Set<Integer>> freePanelists = ScheduleData.instance().getPanelistsUnscheduledDays();
         int counter = 0;
         int total = 0;
         for(String pnst : freePanelists.keySet()) {
             counter++;
             total += freePanelists.get(pnst).size();
-            result.append("\n").append(counter).append(") ").append(pnst).append(" in days ").append(freePanelists.get(pnst));
+            result.append("\n\n").append(counter).append(") ").append(pnst).append(" in days ").append(freePanelists.get(pnst));
         }
         result.append("\n\nTotal Panelist×Day violations: ").append(total);
+
+
+        result.append("\n\n---------------------------- Warning Messages ---------------------------");
+        for(String mes : ScheduleData.instance().getWarningMessages()) {
+            result.append("\n\nWARNING: ").append(mes);
+        }
+
         return result.toString();
     }
 
@@ -40,23 +47,23 @@ public class Report {
         JSONArray scheduled = new JSONArray();
         for(Panel panel : ScheduleData.instance().getAssignedPanels()) {
             JSONObject panelObj = new JSONObject();
-            panelObj.put("panel", panel.NAME);
+            panelObj.put("Panel", panel.NAME);
             panelObj.put("Venue", panel.getVenueTime().VENUE.NAME);
             panelObj.put("Time", TimeFormat.absoluteToNormal(panel.getVenueTime().TIME));
             panelObj.put("Messages", panel.getMessages());
             scheduled.add(panelObj);
         }
-        root.put("scheduled", scheduled);
+        root.put("Scheduled", scheduled);
 
         // unscheduled Panels
         JSONArray unscheduled = new JSONArray();
         for(Panel panel : ScheduleData.instance().getUnschedulablePanels()) {
             JSONObject panelObj = new JSONObject();
-            panelObj.put("panel", panel.NAME);
+            panelObj.put("Panel", panel.NAME);
             panelObj.put("Messages", panel.getMessages());
             unscheduled.add(panelObj);
         }
-        root.put("unscheduled", unscheduled);
+        root.put("Unscheduled", unscheduled);
 
         // Free Panelists
         JSONObject minViolations = new JSONObject();
@@ -66,7 +73,14 @@ public class Report {
             pnstDays.addAll(freePanelists.get(pnst));
             minViolations.put(pnst, pnstDays);
         }
-        root.put("underutilized_panelists", minViolations);
+        root.put("Underutilized_Panelists", minViolations);
+
+        // Warning Messages
+        JSONObject warningMessages = new JSONObject();
+        for(String mes : ScheduleData.instance().getWarningMessages()) {
+            warningMessages.put("WARNING", mes);
+        }
+        root.put("Warning Messages", warningMessages);
 
         return root;
     }

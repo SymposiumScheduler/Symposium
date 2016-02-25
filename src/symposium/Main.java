360f23e7ed718476189cca82f810f4d91e77ad0d
roki7063 <roberto.kingsley@gmail.com>
2016-02-24 18:20:38 -0700

    Minor changes for maintainability

--- src/symposium/Main.java
+++ src/symposium/Main.java
@@ -54,7 +54,7 @@ public class Main {
         int[][] provisionaladjustment = new int[10][5];
 
         for (int i = 0; i < 3; i++) {
-            for (int j = 0; j < 4; j++) {
+            for (int j = 0; j < adjust.length; j++) {
                 optimaladjust[i][j] = 1;
             }
             for (int j=0; j < 2; j++) {
@@ -70,14 +70,14 @@ public class Main {
             adjust[4] = optimaladjust[0][4];
 
             for (int j = 0; j < 10; j++) {
-                for (int k = 0; k < 4; k++) {
+                for (int k = 0; k < adjust.length; k++) {
                     provisionaladjustment[j][k] = (int) Math.round(Math.random());
                 }
 
                 Parser.parse(inputFilePath);
                 // Schedule data is initiated
-                for (int m = 0; m < 4; m++) {
-                    adjust[m] = adjust[m] * (int) Math.pow(16, provisionaladjustment[j][m]);
+                for (int m = 0; m < adjust.length; m++) {
+                    adjust[m] = adjust[m] * (int) Math.pow(8, provisionaladjustment[j][m]);
                     if (adjust[m] == 0) {
                         adjust[m] = 1;
                     }
@@ -105,6 +105,7 @@ public class Main {
                         optimaladjust[m][1] = adjust[1];
                         optimaladjust[m][2] = adjust[2];
                         optimaladjust[m][3] = adjust[3];
+                        optimaladjust[m][4] = adjust[4];
                     }
                 }
                 ScheduleData.deleteScheduleData();
@@ -117,6 +118,7 @@ public class Main {
         System.out.println("Scale for Venue = " + optimaladjust[0][1]);
         System.out.println("Scale for Time = " + optimaladjust[0][2]);
         System.out.println("Scale for Availability = " + optimaladjust[0][3]);
+        System.out.println("Scale for Panelist = " + optimaladjust[0][4]);
 
         System.out.println("\n-------------------THE RESULTS BY DECREASING EITHER VIOLATIONS AND UNSCHEDULED-------------------");
         System.out.println("Violations = " + optimalscore[1][0]);
@@ -125,6 +127,7 @@ public class Main {
         System.out.println("Scale for Venue = " + optimaladjust[1][1]);
         System.out.println("Scale for Time = " + optimaladjust[1][2]);
         System.out.println("Scale for Availability  = " + optimaladjust[1][3]);
+        System.out.println("Scale for Panelist = " + optimaladjust[1][4]);
 
         System.out.println("\n-------------------THE RESULTS BY DECREASING ONLY VIOLATIONS-------------------");
         System.out.println("Violations = " + optimalscore[2][0]);
@@ -133,6 +136,7 @@ public class Main {
         System.out.println("Scale for Venue = " + optimaladjust[2][1]);
         System.out.println("Scale for Time = " + optimaladjust[2][2]);
         System.out.println("Scale for Availability = " + optimaladjust[2][3]);
+        System.out.println("Scale for Panelist = " + optimaladjust[2][4]);
     }
 
     private static String __debugStats() {

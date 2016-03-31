16f56e32593b3656598d67ebe923db9e56e981a1
Joey Rener <joseph.rener@colorado.edu>
2016-03-31 12:59:29 -0600

    .

--- src/symposium/model/Panel.java
+++ src/symposium/model/Panel.java
@@ -13,12 +13,20 @@ public class Panel implements Comparable<Panel>{
     public final List<Constraint> CONSTRAINTS;
     public final List<String> CATEGORIES;
     public final boolean LOCKED;
-
-
     private final List<String> messages;
     private VenueTime assignedVenueTime;
     private int difficulty;
 
+    /**
+     * After setiing all of the variables to the given values, check for filters and set locked to TRUE if a filter is found.
+     * Locked is set to show that this panel must be placed at a certain time or in a certain panel, significantly
+     * increasing its difficulty.
+     * @param name of the panel
+     * @param panelists are the people assigned to the panel
+     * @param availability an intersection of available times from the panelists (calculated in Parser)
+     * @param categories the categories of the panel
+     * @param constraintsStrs a string list of the names of the constraints assigned to the panel; converted to actual constraints below
+     */
     public Panel(String name, List<String> panelists, Range availability, List<String> categories, List<String> constraintsStrs){
         this.NAME = name;
         this.CATEGORIES = categories;

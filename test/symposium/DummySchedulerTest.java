7127b0b9de6d8eec616be2b15667e106e4ba3868
roki7063 <roberto.kingsley@gmail.com>
2016-04-24 19:32:07 -0600

    Split and added tests.

--- test/symposium/DummySchedulerTest.java
+++ test/symposium/DummySchedulerTest.java
@@ -95,7 +95,6 @@ public class DummySchedulerTest {
                 assertTrue(bs.venueConstraintDifficulty(p) > 10000 );
             } else if (p.toString().contains("Performance - Rene")) {
                 assertTrue(p.getDifficulty() > 2000);
-                System.out.println(bs.sizeConstraintDifficulty(p));
                 assertTrue(bs.availabilityDifficulty(p) > 5000);
                 assertTrue(bs.sizeConstraintDifficulty(p) == 10);
                 assertTrue(bs.TimeConstraintDifficulty(p) == 0);
@@ -107,7 +106,7 @@ public class DummySchedulerTest {
     }
 
     @Test
-    public void testMessages() throws Exception {
+    public void testMessagesAssigned() throws Exception {
         String inputFilePath = "datas/dataTest.json";
         int[] diffValues = new int[5];
         diffValues[0] = 10;
@@ -123,6 +122,25 @@ public class DummySchedulerTest {
 
         assertTrue(ScheduleData.instance().getAssignedPanels().get(0).toString().contains("New Panelist Constraint"));
         assertTrue(ScheduleData.instance().getAssignedPanels().get(0).toString().contains("Athena Edmonds [14624], Aaron Balick [14628]"));
+
+        ScheduleData.deleteScheduleData();
+    }
+
+    @Test
+    public void testMessagesUnschedulable() throws Exception {
+        String inputFilePath = "datas/dataTest.json";
+        int[] diffValues = new int[5];
+        diffValues[0] = 10;
+        diffValues[1] = 100000;
+        diffValues[2] = 100000;
+        diffValues[3] = 10000000;
+        diffValues[4] = 100;
+
+        Parser.parse(inputFilePath);
+
+        DummyScheduler bs = new DummyScheduler(diffValues);
+        bs.makeSchedule();
+
         assertTrue(ScheduleData.instance().getUnschedulablePanels().get(0).toString().contains("Venue Time Duration Filter"));
         assertTrue(ScheduleData.instance().getUnschedulablePanels().get(0).toString().contains("SpecificTimeFilter"));
         assertTrue(ScheduleData.instance().getUnschedulablePanels().get(0).toString().contains("1 times"));
